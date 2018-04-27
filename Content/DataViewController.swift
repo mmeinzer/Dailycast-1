695d9c9121250e419217ee4cd6264942173d649a
Adam Barr-Neuwirth <slugiscool99@users.noreply.github.com>
2018-04-26 23:39:57 -0700

    started work on swipe up feature

--- Content/DataViewController.swift
+++ Content/DataViewController.swift
@@ -12,30 +12,18 @@ import SwiftyJSON
 import SwiftyGif
 import Kingfisher
 
-extension UIImage {
-    var averageColor: UIColor? {
-        guard let inputImage = CIImage(image: self) else { return nil }
-        let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
-        
-        guard let filter = CIFilter(name: "CIAreaAverage", withInputParameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return nil }
-        guard let outputImage = filter.outputImage else { return nil }
-        
-        var bitmap = [UInt8](repeating: 0, count: 4)
-        let context = CIContext(options: [kCIContextWorkingColorSpace: kCFNull])
-        context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: kCIFormatRGBA8, colorSpace: nil)
-        
-        return UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: CGFloat(bitmap[3]) / 255)
-    }
-}
 
 class DataViewController: UIViewController {
 
+    @IBOutlet weak var arrowImage: UIImageView!
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: String = ""
     var headlineSnippet: String = ""
     var index: Int?
+    
+    var swipeUp: UIPanGestureRecognizer!
 
     var animatedView: AnimatedImageView!
     
@@ -52,8 +40,14 @@ class DataViewController: UIViewController {
         topLabel.text = dataObject
         topLabel.layer.zPosition = 1
         dataLabel.layer.zPosition = 1
+        arrowImage.layer.zPosition = 1
 
         topLabel.backgroundColor = UIColor.black
+        
+        swipeUp = UIPanGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+        
+        view.addGestureRecognizer(swipeUp)
+        
         setBackground()
     }
 
@@ -75,6 +69,15 @@ class DataViewController: UIViewController {
     }
     
     
+    @objc func swipeGesture(gesture: UIGestureRecognizer){
+//        guard let panRecognizer = gesture as? UIPanGestureRecognizer else {
+//            return super.gestureRecognizerShouldBegin(gesture)
+//        }
+//        let velocity = panRecognizer.velocity(in: self)
+//        if abs(velocity.y) > abs(velocity.x) {
+//            dump(gesture.location(in: view))
+//        }
+    }
     
     func setBackground(){
         print("setting background at index \(index!)")
@@ -82,15 +85,7 @@ class DataViewController: UIViewController {
         animatedView.kf.indicatorType = .activity
         animatedView.kf.setImage(with: gifURLs[index!])
 
-//        if let nextGif = [gifURLs[index!+1]]{
-//            let prefetcher = ImagePrefetcher(urls: [nextGif]) {
-//                skippedResources, failedResources, completedResources in
-//                print("Prefetched: \(completedResources.count)")
-//                print("Skipped: \(skippedResources.count)")
-//                print("Failed: \(failedResources.count)")
-//            }
-//            prefetcher.start()
-//        }
+
 
 
     }
